5d3b95cfd26dff38fce5a2c8080a51efe5a342fb
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-02 13:01:18 +0530

    landing page and job search page had minor bugs which have been fixed

--- src/JobPortal.js
+++ src/JobPortal.js
@@ -53,6 +53,7 @@ const JobPortal = () => {
             value={searchQuery}
             onChange={handleInputChange}
             className="px-4 py-2 border border-gray-300 rounded-md w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            autoFocus
           />
         </form>
       </header>
